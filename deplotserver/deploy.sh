ae5f8c292af9cc49a84c440949ea0da3b28b6246
czc0927 <963239044@qq.com>
2019-05-02 01:32:22 +0800

    12

--- deplotserver/deploy.sh
+++ deplotserver/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+su -
 kill -9 $(pgrep web)
 cd /home/work/server/go/src/lottery
 git clean  -d  -fx
 git pull https://github.com/czc0927/go-lottery.git
-cd web/
+cd /home/work/server/go/src/lottery/web/
 nohup ./web >> ./output.log 2>&1 &
-
 #CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
