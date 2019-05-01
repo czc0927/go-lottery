61283d6e9dfef4a6e8c8efeb806808e73fbbd67c
czc0927 <963239044@qq.com>
2019-05-02 01:38:57 +0800

    test

--- web/build.sh
+++ web/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
+cd ../
+git config  user.name "czc0927"
+git config  user.email "963239044@qq.com"
 git add .
 git commit -m 'test'
 git push
