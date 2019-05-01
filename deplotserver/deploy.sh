6991b90572f746a48568a73ac4cdaa0f91dc9fb8
czc0927 <963239044@qq.com>
2019-05-02 00:59:31 +0800

    firx

--- deplotserver/deploy.sh
+++ deplotserver/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 kill -9 $(pgrep web)
 cd /home/work/server/go/src/lottery
+git clean  -d  -fx
 git pull https://github.com/czc0927/go-lottery.git
 cd web/
 nohup ./web >> ./output.log 2>&1 &
